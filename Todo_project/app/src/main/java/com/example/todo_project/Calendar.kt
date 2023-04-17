c4d45a37dc552365639d6e0f2684bdd76ff4d4cf
설찬우 <cksdn1092@naver.com>
2023-04-17 22:36:13 +0900

    캘린더 작업중

--- Todo_project/app/src/main/java/com/example/todo_project/Calendar.kt
+++ Todo_project/app/src/main/java/com/example/todo_project/Calendar.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CalendarView
+import android.widget.EditText
 import android.widget.TextView
 import androidx.compose.ui.graphics.Color
 import androidx.fragment.app.Fragment
@@ -17,7 +19,8 @@ class Calendar: Fragment(R.layout.calendar) {
     private var _binding: CalendarBinding ?= null
     private lateinit var calendar: CalendarView
     private lateinit var cal_data: TextView
-
+    private lateinit var btn: Button
+    private lateinit var edit: EditText
     //view
     private val binding get() = _binding
 
@@ -31,6 +34,8 @@ class Calendar: Fragment(R.layout.calendar) {
         //뷰 연결
         calendar = _binding!!.calendar
         cal_data = _binding!!.calData
+        edit = _binding!!.edit
+        btn = _binding!!.calBtn
 
 
         //캘린더뷰
@@ -38,7 +43,7 @@ class Calendar: Fragment(R.layout.calendar) {
             cal_data.setText(String.format("%d  /  %d   %d",year,month+1, dayOfMonth))
         })
 
-
+        
         val view = binding?.root
 
         return view
